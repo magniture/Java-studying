03da5e94f612156816ecb02b072b194d9f135fba
111111111111 <48746658+magniture@users.noreply.github.com>
2022-08-14 20:35:26 +0800

    1

--- edu-boss/src/services/options.js
+++
@@ -1,7 +0,0 @@
-/**
- * Options service
- */
-
-import ResourceUtil from './resourceUtil'
-
-export default new ResourceUtil(process.env.VUE_APP_API_FAKE + '/options')
--- edu-boss/src/store/modules/index.js
+++
@@ -1,7 +0,0 @@
-import courses from './courses'
-import sections from './sections'
-import lessons from './lessons'
-import users from './users'
-import options from './options'
-
-export default { courses, sections, lessons, users, options }
