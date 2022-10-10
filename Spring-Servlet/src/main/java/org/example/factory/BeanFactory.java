db02fe55cc434d8ada8ab39195c0f66fe76eaddc
zhangqi <2334521489@qq.com>
2022-10-10 11:32:08 +0800

    通过反射获取数据

--- Spring-Servlet/src/main/java/org/example/factory/BeanFactory.java
+++ Spring-Servlet/src/main/java/org/example/factory/BeanFactory.java
@@ -8,12 +8,22 @@ import org.example.service.impl.DemoServiceImpl;
 public class BeanFactory {
 
     public static DemoService getDemoService() {
-        return new DemoServiceImpl();
+        try {
+            return (DemoService) Class.forName("com.linkedbear.architecture.c_reflect.service.impl.DemoServiceImpl")
+                    .newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("DemoService instantiation error, cause: " + e.getMessage());
+        }
     }
 
     public static DemoDao getDemoDao() {
-//        return new DemoDaoImpl();
-        return new DemoMySQLDao();
+        try {
+            return (DemoDao) Class.forName("com.linkedbear.architecture.c_reflect.dao.impl.DemoDaoImpl").newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("DemoDao instantiation error, cause: " + e.getMessage());
+        }
     }
 
 }
