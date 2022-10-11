419e533b04324a012d5173e78d61cfad6df9c0b8
zhangqi <2334521489@qq.com>
2022-10-11 13:39:35 +0800

    在类上标注 @Component 注解，即代表该类会被注册到 IOC 容器中作为一个 Bean

---
+++ Spring-ioc/src/main/java/com/zq/spring/annotation/b_scan/bean/Person.java
@@ -0,0 +1,7 @@
+package com.zq.spring.annotation.b_scan.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+}
---
+++ Spring-ioc/src/main/java/com/zq/spring/annotation/b_scan/config/ComponentScanConfiguration.java
@@ -0,0 +1,9 @@
+package com.zq.spring.annotation.b_scan.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.zq.spring.annotation.b_scan")
+public class ComponentScanConfiguration {
+}
