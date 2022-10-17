009598c48a03371f1ef53dd061c7773387c11287
zhangqi <2334521489@qq.com>
2022-10-17 13:48:14 +0800

    bean作用域

--- spring-01-ioc/src/main/java/com/linkedbear/spring/bean/b_scope/bean/Toy.java
+++ spring-01-ioc/src/main/java/com/linkedbear/spring/bean/b_scope/bean/Toy.java
@@ -3,6 +3,7 @@ package com.linkedbear.spring.bean.b_scope.bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+//@Scope ，并声明为原型类型
 @Component
 @Scope("prototype") // ConfigurableBeanFactory.SCOPE_SINGLETON
 public class Toy {
