58df87884b11442398cdf964c635e775e8aa0810
zhangqi <2334521489@qq.com>
2022-10-25 09:31:59 +0800

    观察者模式,体会事件与监听器

--- spring-01-ioc/src/main/java/com/linkedbear/spring/event/a_quickstart/QuickstartListenerApplication.java
+++ spring-01-ioc/src/main/java/com/linkedbear/spring/event/a_quickstart/QuickstartListenerApplication.java
@@ -13,3 +13,6 @@ public class QuickstartListenerApplication {
         System.out.println("IOC容器关闭。。。");
     }
 }
+//ApplicationListener 会在容器初始化阶段就准备好，在容器销毁时一起销毁；
+//ApplicationListener 也是 IOC 容器中的普通 Bean ；
+//IOC 容器中有内置的一些事件供我们监听。
