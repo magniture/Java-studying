6a26c5ba2dc9a1ed98a48e9caaf6d488b46c4c6b
zhangqi <2334521489@qq.com>
2022-10-25 09:25:24 +0800

    观察者模式,体会事件与监听器

--- spring-01-ioc/src/main/java/com/linkedbear/spring/event/a_quickstart/listener/ContextRefreshedApplicationListener.java
+++ spring-01-ioc/src/main/java/com/linkedbear/spring/event/a_quickstart/listener/ContextRefreshedApplicationListener.java
@@ -3,7 +3,8 @@ package com.linkedbear.spring.event.a_quickstart.listener;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-
+//内置的监听器接口ApplicationListener
+//ContextRefreshedEvent 和 ContextClosedEvent ，它们分别代表容器刷新完毕和即将关闭
 @Component
 public class ContextRefreshedApplicationListener implements ApplicationListener<ContextRefreshedEvent> {
     
