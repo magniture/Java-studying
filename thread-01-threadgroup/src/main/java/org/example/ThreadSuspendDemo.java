e503fda79768e23b8e8dc98bf97822196720b1fd
zhangqi <2334521489@qq.com>
2022-11-03 15:41:24 +0800

    线程暂停-suspend

---
+++ thread-01-threadgroup/src/main/java/org/example/ThreadSuspendDemo.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @BelongsProject: thread-01-threadgroup
+ * @BelongsPackage: org.example
+ * @Author: zhangq
+ * @CreateTime: 2022-11-03  15:36
+ * @Description: 终止线程的手段-suspend
+ */
+public class ThreadSuspendDemo {
+
+    static class SuspendThread implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 100 ;i++) {
+                System.out.println(i+"");
+                try {
+                    Thread.sleep(1000);
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread suspendThread = new Thread(new SuspendThread());
+        suspendThread.start();
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println("线程暂停");
+
+        suspendThread.suspend();
+        TimeUnit.SECONDS.sleep(2);
+        //从之前暂停的位置恢复继续执行
+        suspendThread.resume();
+        System.out.print(" 线程恢复");
+    }
+}
