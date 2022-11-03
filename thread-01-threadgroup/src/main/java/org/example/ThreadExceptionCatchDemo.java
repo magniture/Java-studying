8776633686da9eeda510a0df8c7e132db4b6a6c9
zhangqi <2334521489@qq.com>
2022-11-03 15:27:22 +0800

    线程异常监控

---
+++ thread-01-threadgroup/src/main/java/org/example/ThreadExceptionCatchDemo.java
@@ -0,0 +1,32 @@
+package org.example;
+
+/**
+ * @BelongsProject: thread-01-threadgroup
+ * @BelongsPackage: org.example
+ * @Author: zhangq
+ * @CreateTime: 2022-11-01  10:34
+ * @Description: TODO
+ */
+public class ThreadExceptionCatchDemo {
+    public static  void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("this is test");
+                int i = 10/0;
+            }
+        });
+
+        //是对线程进行监控，只要是异常就会回调到UncaughtExceptionHandler
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler(){
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("thread is"+ t.getName());
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+
+
+}
