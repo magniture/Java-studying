3e6d646e75c7cb4f01026429587f91410f7563a1
zhangqi <2334521489@qq.com>
2022-11-15 10:21:17 +0800

    Spring整合jdbc

--- spring-03-dao/src/main/java/com/linkedbear/spring/jdbc/a_quickstart/JdbcTemplateXmlApplication.java
+++ spring-03-dao/src/main/java/com/linkedbear/spring/jdbc/a_quickstart/JdbcTemplateXmlApplication.java
@@ -2,7 +2,11 @@ package com.linkedbear.spring.jdbc.a_quickstart;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
-
+/**
+ * @Author Zhang Q
+ * @Description  Spring整合jdbc
+ * @Date 10:19 2022/11/15
+ **/
 public class JdbcTemplateXmlApplication {
     
     public static void main(String[] args) throws Exception {
