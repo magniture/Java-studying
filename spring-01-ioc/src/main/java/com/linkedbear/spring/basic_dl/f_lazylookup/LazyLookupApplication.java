9d2d727133557d0b7c9ca7b1a45f338ea53d032d
zhangqi <2334521489@qq.com>
2022-12-06 15:37:49 +0800

    延迟查询getBeanProvider

--- spring-01-ioc/src/main/java/com/linkedbear/spring/basic_dl/f_lazylookup/LazyLookupApplication.java
+++ spring-01-ioc/src/main/java/com/linkedbear/spring/basic_dl/f_lazylookup/LazyLookupApplication.java
@@ -19,7 +19,12 @@ public class LazyLookupApplication {
         // 这一行代码不会报错
         ObjectProvider<Dog> dogProvider = ctx.getBeanProvider(Dog.class);
         // 只有当执行getObject方法时才会检查是否存在，即实现了延迟查找
-        System.out.println(dogProvider.getObject());
+        Dog dog = dogProvider.getIfAvailable();
+        if (dog != null) {
+            System.out.println(dogProvider.getObject());
+
+        }
+//        System.out.println(dogProvider.getObject());
         
         // jdk8后ObjectProvider接口有了新的扩展：借助Supplier接口返回缺省创建
         Dog defaultDog = dogProvider.getIfAvailable(Dog::new);
