088d8068459a665b4e6594bd1a0e98ead3ff604f
zhangqi <2334521489@qq.com>
2022-12-21 17:34:15 +0800

    学习ElasticSearch

---
+++ elasticSearch/src/main/java/com/example/elasticsearch/ElasticSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchApplication.class, args);
+	}
+
+}
