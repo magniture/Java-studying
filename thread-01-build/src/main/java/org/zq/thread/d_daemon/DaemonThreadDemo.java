58dd96e81833d14b65a66b9dce2a3581b55d9aa6
zhangqi <2334521489@qq.com>
2022-12-26 13:49:36 +0800

    守护线程

---
+++ thread-01-build/src/main/java/org/zq/thread/d_daemon/DaemonThreadDemo.java
@@ -0,0 +1,28 @@
+package org.zq.thread.d_daemon;
+
+/**
+ * @BelongsProject: Java-studying
+ * @BelongsPackage: org.zq.thread.d_daemon
+
+ * @Description: 守护线程
+ */
+public class DaemonThreadDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println("jvm exit success!! ")));
+
+        Thread testThread = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(2000);
+                    System.out.println("thread still running ....");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        testThread.setDaemon(true); //可以把这个注释看看
+        testThread.start();
+        //守护线程具有在JVM退出的时候也自我销毁的特点，而非守护线程不具备这个特点，这也是为什么GC回收线程被设置为守护线程类型的主要原因。
+    }
+}
