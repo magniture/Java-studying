dc9bef47c032fbaea1f7393160ba3fab2a4763fd
zhangqi <2334521489@qq.com>
2022-12-26 13:20:43 +0800

    线程的构建

---
+++ thread-01-build/src/main/java/org/zq/thread/a_build/Main.java
@@ -0,0 +1,25 @@
+package org.zq.thread.a_build;
+
+/**
+ * @BelongsProject: Java-studying
+ * @BelongsPackage: org.zq.thread.a_build
+ * @Author: zhangq
+ * @CreateTime: 2022-12-26  13:17
+ * @Description: 线程的构建
+ */
+public class Main {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            while (true) { //一直为true，循环
+                try {
+                    Thread.sleep(10000); //线程等待10s
+                    System.out.println("Thread is runnning");
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+    }
+}
